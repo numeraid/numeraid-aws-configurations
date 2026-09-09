ce05f1c3d9fe71ac67130837dbdf6f25b07185f7
devzwide <nxumalobukeka66@gmail.com>
2026-09-09 09:53:59 +0200

    Refactor Terraform configuration files and structure for improved organization and clarity

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,11 +1,3 @@
-/*
-
-Although shared modules inherit providers from calling modules, modules should not configure provider settings themselves. 
-Avoid specifying provider configuration blocks in modules. 
-This configuration should only be declared once globally.
-
-*/
-
 provider "aws" {
   region = var.aws_region
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,14 @@
-/*
-
-Guidelines for variable naming
-
-- Add units to the names of inputs, local variables, and outputs that represent numeric values such as disk size or RAM size (for example, ram_size_gb for RAM size in gigabytes). This practice makes the expected input unit clear for configuration maintainers.
-- Use binary units such as MiB and GiB for storage sizes, and decimal units such as MB or GB for other metrics.
-- Give Boolean variables positive names such as enable_external_access.
-
-*/
-
 variable "aws_region" {
   description = "AWS Region where resources will be deployed"
-  type = string
+  type        = string
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
 }
